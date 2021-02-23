25b1dcf53afeb4bbb83b4ed6a1dd0348c1d5395f
Daniel Santos <danvaini@outlook.com>
2021-02-22 23:18:27 -0300

    feat: Aprendendo sobre strings, fatiamento e função print

---
+++ string.py
@@ -0,0 +1,43 @@
+print("doesn't")
+
+# Quebra de linha
+
+print('Daniel \n Santos')
+
+# Quando queremos evitar a quebra de linha
+
+print('c:\name')
+print(r'c:\name')
+
+# Strings Longas
+
+print("""
+  Olá pequenos gafanhotos,
+  linha2
+  linha3
+""")
+
+
+# Operadores com string
+
+print(3 * 'dan')
+
+# concatenação
+
+print('Py''thon') #Junta os dois
+
+
+# Pegar a letra de uma string
+
+nome = 'Daniel dos santos vaini'
+print(nome[4])
+
+# Fatiamento
+
+print(nome[0:10])
+print(nome[0:4])
+print(nome[0:18])
+
+# Fatiamento + concatenação
+
+print(nome[:10] + nome[10:])

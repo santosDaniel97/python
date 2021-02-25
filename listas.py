b1a53e09c1163f0c5acff12a4a4c46ea403d7722
Daniel Santos <danvaini@outlook.com>
2021-02-25 10:56:34 -0300

    feat: Aprendendo como manipular listas em python

--- listas.py
+++ listas.py
@@ -32,3 +32,29 @@ print(letras)
 # Método len
 
 print(len(letras))
+
+
+# Método append
+
+letras.append('F')
+
+# Append com valor digitado pelo usuário 
+
+letra = input('Digite algo para colocar no array')
+letras.append(letra)
+print(letras)
+
+# Listas dentro de listas
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = [a, b]
+
+print(c)
+
+# estrutura de repetição
+
+a, b = 0, 1
+while a < 1000: 
+  print(a, end=",")
+  a, b = b, a+b
